fccb1298dc892c5d1963eeec44b6c9d2c08e389a
Ryszard Schossler <ryszard.schossler@gmail.com>
2023-06-01 14:41:24 +0200

    navigation between pages, turn off transition anim

--- lib/screens/resume_screen.dart
+++ lib/screens/resume_screen.dart
@@ -1,13 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:personal_website/widgets/nav_bar.dart';
 
 class ResumeScreen extends StatelessWidget {
   const ResumeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Resume'),
+    return Scaffold(
+      body: Column(
+        children: const [
+          NavBar(),
+          Center(
+            child: Text('Resume'),
+          ),
+        ],
       ),
     );
   }
